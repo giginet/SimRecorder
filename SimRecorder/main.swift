0396354d3a8980652022e689eeb749352de77086
giginet <giginet.net@gmail.com>
2015-10-15 00:23:09 +0900

    Initial Commit

---
+++ SimRecorder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimRecorder
+//
+//  Created by giginet on 10/15/15.
+//  Copyright © 2015 giginet. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
